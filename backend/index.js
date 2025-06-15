191346f8bcf0979569721fb342452d38d2ddc119
adityamishra009 <adityamishra0905@gmail.com>
2025-06-15 20:02:49 +0530

    join room created aditya

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,45 @@
 import express from 'express'
 import http from 'http'
+import { Server } from 'socket.io';
 
 const app= express()
 
 const server = http.createServer(app);
 
-const port = process.env.PORT || 5000;
+const io = new Server(server,{
+  cors: {
+    origin:"*",
+  },
+});
+
+const rooms = new Map();
+
+io.on("connection",(socket)=>{
+  console.log("user connected",socket.id);
+
+  let currentRoom=null;
+  let currentUser=null;
+
+  socket.on("join",({roomid,username})=>{
+    if(currentRoom){
+      socket.leave(currentRoom)
+      rooms.get(currentRoom).delete(currentUser);
+      io.to(currentRoom).emit("userjoined",Array.from(rooms.get(currentRoom)));
+    }
+    currentRoom=roomid;
+    currentUser=username;
+
+    socket.join(roomid)
+    if(!rooms.has(roomid)){
+      rooms.set(roomid,new Set());
+    }
+    rooms.get(roomid).add(username)
+    io.to(roomid).emit("userjoined",Array.from(rooms.get(currentRoom)));
+
+
+  });
+});
+const port = process.env.PORT || 5173;
 
 server.listen(port,()=>{
   console.log('server is working');
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import "./App.css";
+import io from 'socket.io-client'
+const socket = io("http://localhost:5173/");
+const App = () => {
+  const [joined,setjoined]=useState(false);
+  const[RoomId,setRoomId]=useState("");
+  const[UserName,setUserName]=useState("");
+  if(!joined){
+     return <div className="join-container">
+    <div className="join-form">
+      <h1>Join Code Room</h1>
+      <input type="text" placeholder="Room Id"
+      value={RoomId}
+      onChange={(e)=>setRoomId(e.target.value)}/>
+      <input type="text" placeholder="Your Name"
+      value={UserName}
+      onChange={(e)=>setUserName(e.target.value)}/>
+      <button>join Room</button>
+    </div>
+  </div>;
+  }
+};
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
