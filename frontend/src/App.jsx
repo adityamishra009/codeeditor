dc93ac1c7be858e330d9fc3a81ea84fde57d19ce
Ankit Pandey <ankit221601@gmail.com>
2025-06-25 17:15:15 +0530

    modified backend api in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import io from 'socket.io-client';
 import Editor from '@monaco-editor/react';
 
-
+// it is backend api 
 const socket = io("http://localhost:3000/");
 
 const App = () => {
